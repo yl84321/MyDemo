d85ef7cd8f7f6af072be29e41f7bc511be4e8d41
yao <358767228@qq.com>
2021-11-08 14:24:43 +0800

    打发打发

---
+++ app/src/main/java/com/example/mydemo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.mydemo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //得到开发进度款减肥
+    }
+}
